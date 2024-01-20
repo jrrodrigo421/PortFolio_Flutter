198c2a083ddd730450266f1e6b951b8162ed181c
jrrodrigo421 <rjr89000@gmail.com>
2024-01-20 11:23:22 -0300

    feat: background image

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -62,103 +62,115 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.only(
-            top: size.height * 0.2,
-            left: size.height * 0.2,
-            right: size.height * 0.2),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Center(
-                        child: FadeInDown(
-                          duration: Duration(milliseconds: 2000),
-                          child: Text(
-                            'Olá, seja bem-vindo,',
-                            style: ApptextStyles.montseratStyle(
-                                color: Colors.white),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg1.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SingleChildScrollView(
+          padding: EdgeInsets.only(
+              top: size.height * 0.2,
+              left: size.height * 0.2,
+              right: size.height * 0.2),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Center(
+                          child: FadeInDown(
+                            duration: const Duration(milliseconds: 2000),
+                            child: Text(
+                              'Olá, seja bem-vindo,',
+                              style: ApptextStyles.montseratStyle(
+                                  color: Colors.white),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Constants.sizedBox(height: 15.0),
-                    FadeInRight(
-                      duration: Duration(milliseconds: 2000),
-                      child: Text(
-                        'Meu nome é Rodrigo Lopes',
-                        style: ApptextStyles.headingStyles(),
-                      ),
-                    ),
-                    Constants.sizedBox(height: 15.0),
-                    Row(
-                      children: [
-                        Text(
-                          'Sou ',
-                          style:
-                              ApptextStyles.montseratStyle(color: Colors.white),
+                      Constants.sizedBox(height: 15.0),
+                      FadeInRight(
+                        duration: const Duration(milliseconds: 2000),
+                        child: Text(
+                          'Meu nome é Rodrigo Lopes',
+                          style: ApptextStyles.headingStyles(),
                         ),
-                        AnimatedTextKit(
-                          animatedTexts: [
-                            TyperAnimatedText(
-                              'Desenvolvedor de Software',
-                              textStyle: ApptextStyles.montseratStyle(
-                                  color: Colors.limeAccent),
-                            ),
-                            TyperAnimatedText(
-                              'SantaRitense',
-                              textStyle: ApptextStyles.montseratStyle(
-                                  color: Colors.limeAccent),
-                            ),
-                            TyperAnimatedText(
-                              'curioso por tudo',
-                              textStyle: ApptextStyles.montseratStyle(
-                                  color: Colors.limeAccent),
-                            ),
-                          ],
-                          pause: const Duration(milliseconds: 1000),
-                          displayFullTextOnTap: true,
-                          stopPauseOnTap: true,
-                        )
-                      ],
-                    ),
-                    Constants.sizedBox(height: 15.0),
-                    SizedBox(
-                      width: size.width * 0.5,
-                      child: Text(''' 
-Apaixonado por tecnologia, motivado pela curiosidade e com vontade de sempre querer avançar mais!
-Já fui do ramo administrativo/comercial, parti para área técnica aprofundei em tecnologias da informação e agora cada vez mais mergulhando no universo da programação.
-
-// Graduado em Tecnólogo de Gestão de Telecomunicações no INATEL
-// Atualmente estudante de Análise e Desenvolvimento de Sistemas na Faculdade Estácio.                          ''',
-                          style: ApptextStyles.normalStyle()),
-                    ),
-                    Constants.sizedBox(height: 22),
-                    Row(
-                      children: [
-                        buildSocialButton(asset: AppAssets.linkedin),
-                        Constants.sizedBox(widht: 12.0),
-                        buildSocialButton(asset: AppAssets.github),
-                        Constants.sizedBox(widht: 12.0),
-                        buildSocialButton(asset: AppAssets.insta),
-                        Constants.sizedBox(widht: 12.0),
-                        buildSocialButton(asset: AppAssets.facebook),
-                        Constants.sizedBox(widht: 12.0),
-                        buildSocialButton(asset: AppAssets.twitter),
-                        Constants.sizedBox(widht: 12.0),
-                      ],
-                    ),
-                    Constants.sizedBox(height: 22),
-                    buildMaterialBuild(onTap: () {}),
-                  ],
-                )
-              ],
-            )
-          ],
+                      ),
+                      Constants.sizedBox(height: 15.0),
+                      Row(
+                        children: [
+                          Text(
+                            'Sou ',
+                            style: ApptextStyles.montseratStyle(
+                                color: Colors.white),
+                          ),
+                          AnimatedTextKit(
+                            animatedTexts: [
+                              TyperAnimatedText(
+                                'Desenvolvedor de Software',
+                                textStyle: ApptextStyles.montseratStyle(
+                                    color: Colors.limeAccent),
+                              ),
+                              TyperAnimatedText(
+                                'SantaRitense',
+                                textStyle: ApptextStyles.montseratStyle(
+                                    color: Colors.limeAccent),
+                              ),
+                              TyperAnimatedText(
+                                'curioso por tudo',
+                                textStyle: ApptextStyles.montseratStyle(
+                                    color: Colors.limeAccent),
+                              ),
+                            ],
+                            pause: const Duration(milliseconds: 1000),
+                            displayFullTextOnTap: true,
+                            stopPauseOnTap: true,
+                            repeatForever: true,
+                          )
+                        ],
+                      ),
+                      Constants.sizedBox(height: 15.0),
+                      SizedBox(
+                        width: size.width * 0.5,
+                        child: Text(''' 
+          Apaixonado por tecnologia, motivado pela curiosidade e com vontade de sempre querer avançar mais!
+          Já fui do ramo administrativo/comercial, parti para área técnica aprofundei em tecnologias da informação e agora cada vez mais mergulhando no universo da programação.
+          
+          // Graduado em Tecnólogo de Gestão de Telecomunicações no INATEL
+          // Atualmente estudante de Análise e Desenvolvimento de Sistemas na Faculdade Estácio.                          ''',
+                            style: ApptextStyles.normalStyle()),
+                      ),
+                      Constants.sizedBox(height: 22),
+                      Row(
+                        children: [
+                          buildSocialButton(asset: AppAssets.linkedin),
+                          Constants.sizedBox(widht: 12.0),
+                          buildSocialButton(asset: AppAssets.github),
+                          Constants.sizedBox(widht: 12.0),
+                          buildSocialButton(asset: AppAssets.insta),
+                          Constants.sizedBox(widht: 12.0),
+                          buildSocialButton(asset: AppAssets.facebook),
+                          Constants.sizedBox(widht: 12.0),
+                          buildSocialButton(asset: AppAssets.twitter),
+                          Constants.sizedBox(widht: 12.0),
+                        ],
+                      ),
+                      Constants.sizedBox(height: 22),
+                      buildMaterialBuild(onTap: () {}),
+                      const SizedBox(
+                        height: 60,
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -197,7 +209,6 @@ Já fui do ramo administrativo/comercial, parti para área técnica aprofundei e
       ),
       padding: const EdgeInsets.all(6),
       child: InkWell(
-        // child: CircleAvatar(
         borderRadius: BorderRadius.circular(700.0),
         hoverColor: AppColors.aqua,
         splashColor: AppColors.lowGreen,
@@ -209,7 +220,6 @@ Já fui do ramo administrativo/comercial, parti para área técnica aprofundei e
           color: AppColors.themeColor,
           fit: BoxFit.fill,
         ),
-        // ),
       ),
     );
   }
